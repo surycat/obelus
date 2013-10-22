da9f8e04c780ca00e8f704d8c6cd931fdf7067f3
Antoine Pitrou <antoine.pitrou@optiflows.com>
2013-10-22 17:26:35 +0200

    Add workaround for unittest bug with symlinks

--- obelus/test/__main__.py
+++ obelus/test/__main__.py
@@ -1,7 +1,19 @@
 import os
+import sys
 import unittest
 
+import obelus.test
 from . import main
+
+
+def rewrite_import_path(path):
+    """
+    Rewrite a sys.path-style list of import paths to resolve all symlinks
+    on absolute directories.
+    """
+    for i, dirname in enumerate(path):
+        if os.path.isabs(dirname):
+            path[i] = os.path.realpath(dirname)
 
 
 def load_tests(loader, standard_tests, pattern):
@@ -9,6 +21,9 @@ def load_tests(loader, standard_tests, pattern):
     this_dir = os.path.dirname(__file__)
     top_level_dir = os.path.dirname(os.path.dirname(this_dir))
     pattern = pattern or "test_*.py"
+    # Workaround for http://bugs.python.org/issue19347
+    rewrite_import_path(sys.path)
+    rewrite_import_path(obelus.test.__path__)
     package_tests = loader.discover(start_dir=this_dir, pattern=pattern,
                                     top_level_dir=top_level_dir)
     standard_tests.addTests(package_tests)
@@ -16,4 +31,4 @@ def load_tests(loader, standard_tests, pattern):
 
 
 if __name__ == '__main__':
-    main()
+    obelus.test.main()
